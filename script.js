dd97997aa29c0cb1b51606dd67fcab4edda30044
HanifAjiPrasetyo <hanifaji007@gmail.com>
2022-10-17 09:41:12 +0700

    1st commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// ACTIVE MENU FUNCTION
+var navCon = document.getElementById("nav-con");
+var navItem = navCon.getElementsByClassName("link");
+for (var i = 0; i < navItem.length; i++) {
+  navItem[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+
+    this.className += " active";
+  });
+}
+
+// SLIDESHOW FUNCTION
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
